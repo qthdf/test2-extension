164a9b92c6b0ef920dc0a136db1d771550c101fc
qthdf <110314206+qthdf@users.noreply.github.com>
2022-08-31 14:38:25 +0800

    Add files via upload

---
+++ enums.d.ts
@@ -0,0 +1,42 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * Andy England @ SparkFun Electronics
+     * June 4, 2019
+     * https://github.com/sparkfun/pxt-gator-particle
+     *
+     * Development environment specifics:
+     * Written in Microsoft PXT
+     * Tested with a SparkFun Gator:Particle Sensor
+     *
+     * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+     * Please review the LICENSE.md file included with this example. If you have any questions
+     * or concerns with licensing, please contact techsupport@sparkfun.com.
+     * Distributed as-is; no warranty is given.
+     */
+
+    declare const enum LEDToRead {
+    //% block="Red" enumval=1
+    Red = 1,
+    //% block="Infrared" enumval=2
+    Infrared = 2,
+    }
+
+
+    declare const enum HeartbeatType {
+    //% block="BPM" enumval=0
+    BPM = 0,
+    //% block="AVG" enumval=1
+    AVG = 1,
+    }
+
+
+    declare const enum LEDMode {
+    //% block="Red" enumval=2
+    RedLED = 2,
+    //% block="Red & Infrared" enumval=3
+    RedAndIR = 3,
+    }
+
+// Auto-generated. Do not edit. Really.
