52453b79bc09a692b7edbf1a59e5475627772b5f
qthdf <110314206+qthdf@users.noreply.github.com>
2022-08-31 14:42:01 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-declare const enum LEDToRead {
-    //% block="Red" enumval=1
-    Red = 1,
-    //% block="Infrared" enumval=2
-    Infrared = 2,
-}
-declare const enum HeartbeatType {
-    //% block="BPM" enumval=0
-    BPM = 0,
-    //% block="AVG" enumval=1
-    AVG = 1,
-}
-declare const enum LEDMode {
-    //% block="Red" enumval=2
-    RedLED = 2,
-    //% block="Red & Infrared" enumval=3
-    RedAndIR = 3,
-}
 enum PingUnit {
     //% block="微秒"
     MicroSeconds,
@@ -115,64 +97,67 @@ namespace Microbit {
 
 
 
-    //% weight=30 
-    //% blockId="gatorParticle_begin" 
-    //% block="心率血氧传感器初始化"
-    //% group="心率血氧传感器" subcategory=传感器
-    //% shim=gatorParticle::begin
-    export function begin() {
-        return
-    }
+    // Functions for reading Particle from the gatorParticle in Particle or straight adv value
+	
+	/**
+	* Initializes the gator:particle sensor, must be called on power up
+	*/	
+	//% weight=30 
+	//% blockId="gatorParticle_begin" 
+	//% block="initialize gator:Particle sensor"
+	//% shim=Microbit::begin
+	export function begin(){
+		return
+	}
+		
+	/**
+	* Reads either the Red or Infrared detection channels
+	*/
+	//% weight=29 
+	//% blockId="gatorParticle_color" 
+	//% block="get %LEDToRead value"
+	//% shim=Microbit::color
+	export function color(type: LEDToRead): number{
+		return 0
+	}
+	
+	/**
+	* Set which LED's we want the sensor to update and read.
+	*/	
+	//% weight=28
+	//% blockId="gatorParticle_setReadMode"
+	//% block="set LED mode to read %LEDMode"
+	//% shim=Microbit::setReadMode
+	//% advanced=true
+	export function setReadMode(mode: LEDMode)
+	{
+		return
+	}
 
-    /**
-    * Reads either the Red or Infrared detection channels
-    */
-    //% weight=29 
-    //% blockId="gatorParticle_color" 
-    //% block="获取 %LEDToRead 值"
-    //% group="心率血氧传感器" subcategory=传感器
-    //% shim=gatorParticle::color
-    export function color(type: LEDToRead): number {
-        return 0
-    }
-
-    /**
-    * Set which LED's we want the sensor to update and read.
-    */
-    //% weight=28
-    //% blockId="gatorParticle_setReadMode"
-    //% block="设置LED的读取模式 %LEDMode"
-    //% group="心率血氧传感器" subcategory=传感器
-    //% shim=gatorParticle::setReadMode
-
-    export function setReadMode(mode: LEDMode) {
-        return
-    }
-
-    /**
-    * Set the amplitude of either Red or Infrared LED
-    */
-    //% weight=27
-    //% blockId="gatorParticle_setAmplitude"
-    //% block="将强度从 %LEDToRead | 设置为 %myBrightness"
-    //% group="心率血氧传感器" subcategory=传感器
-    //% shim=gatorParticle::setAmplitude
-
-    export function setAmplitude(led: LEDToRead, myBrightness: number) {
-        return
-    }
-
-    /**
-    * Grab the heartbeat from the sensor in either beats per minute, or an average of the last 4 BPM readings.
-    */
-    //% weight=26
-    //% blockId="gatorParticle_heartbeat"
-    //% block="设置心跳检测方式为%HeartbeatType"
-    //% group="心率血氧传感器" subcategory=传感器
-    //% shim=gatorParticle::heartbeat
-    export function heartbeat(type: HeartbeatType): number {
-        return 0
-    }
+	/**
+	* Set the amplitude of either Red or Infrared LED
+	*/	
+	//% weight=27
+	//% blockId="gatorParticle_setAmplitude"
+	//% block="change strength of %LEDToRead | to %myBrightness"
+	//% shim=Microbit::setAmplitude
+	//% advanced=true
+	export function setAmplitude(led: LEDToRead, myBrightness: number)
+	{
+		return
+	}
+	
+	/**
+	* Grab the heartbeat from the sensor in either beats per minute, or an average of the last 4 BPM readings.
+	*/
+	//% weight=26
+	//% blockId="gatorParticle_heartbeat"
+	//% block="detect heartbeat in %HeartbeatType"
+	//% shim=Microbit::heartbeat
+	export function heartbeat(type: HeartbeatType): number
+	{
+		return 0
+	}
 
 
 
